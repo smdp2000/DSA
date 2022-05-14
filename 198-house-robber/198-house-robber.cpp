90e0013e3649b8efad0873954fcfb22e4d08462c
Somyadeep Shrivastava <40947644+smdp2000@users.noreply.github.com>
2022-05-14 15:08:12 +0530

    Time: 12 ms (5.27%), Space: 7.7 MB (48.49%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,6 +2,28 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         
+        int rob1=0, rob2=0, cur=0;
+        
+        
+        for(auto i:nums){
+            //int temp = i;
+            cur = max(rob2, rob1+i );
+            rob1 = rob2;
+            rob2 = cur;
+        
+        }
+        
+        return cur;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
         if(nums.size()==0)
             return -1;
         
@@ -13,7 +35,7 @@ public:
         int sumtl[nums.size()];
         
         sumtl[0] = nums[0];
-        sumtl[1] = max(nums[0], nums[1]);
+        sumtl[1] = max(nums[0], nums[1];
         
         
         for(int i=2; i<nums.size(); i++){
@@ -23,7 +45,7 @@ public:
         return sumtl[nums.size()-1];
         
         
-        
+        */
             
     }
 };
