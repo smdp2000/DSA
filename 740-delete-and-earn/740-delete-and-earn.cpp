95590b118da6d19a3b9dcd0ec421ef893be608f4
Somyadeep Shrivastava <40947644+smdp2000@users.noreply.github.com>
2022-06-16 00:17:42 +0530

    Time: 10 ms (92.23%), Space: 11.7 MB (90.04%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -13,14 +13,26 @@ public:
       //  for(int i=0; i<fn; i++)
        //     cout<<freq[i];
                         
-        int cur=0, prev1=0, prev2=0;
+        /*int cur=0, prev1=0, prev2=0;
         for(int i=0; i<fn; i++){
             
             cur = max(prev1+freq[i], prev2);
             prev1 = prev2;
             prev2 = cur;
         }
+        */
+        int dp[fn];
         
-        return cur;
+        dp[0] = freq[0];
+        dp[1] = max(freq[0], freq[1]);
+        
+        for(int i=2; i<fn; i++){
+            
+            dp[i] = max(dp[i-2]+freq[i], dp[i-1]);
+            
+        }
+        
+        
+        return dp[fn-1];
     }
 };
