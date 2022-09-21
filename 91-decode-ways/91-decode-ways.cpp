84ca0560af0bf9fa9aae56d5db8010d04c63244e
Somyadeep Shrivastava <40947644+smdp2000@users.noreply.github.com>
2022-09-21 09:41:13 -0700

    Time: 9 ms (13.83%), Space: 6.3 MB (49.56%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -7,10 +7,11 @@ public:
         vector<int> dp(n+1);
         dp[n] = 1;
         for(int i=n-1;i>=0;i--) {
-            if(s[i]=='0') dp[i]=0;
+            if(s[i]=='0') 
+                dp[i]=0;
             else {
                 dp[i] = dp[i+1];
-                if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) dp[i]+=dp[i+2];
+                if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) dp[i]+=dp[i+2];  //for two digit
             }
         }
         return dp[0]; 
