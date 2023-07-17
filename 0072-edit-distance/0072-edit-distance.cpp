10404a023678badedc298869307a93bc5ceaae98
Somyadeep Shrivastava <40947644+smdp2000@users.noreply.github.com>
2023-07-16 23:43:10 -0700

    Time: 67 ms (21.46%), Space: 82 MB (7.65%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int getOperations(int i, int j, string s1, string s2, vector<vector<int>>&dp){
+        
+        if(i==0) return j;
+        if(j==0) return i;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(s1[i-1] == s2[j-1]) return dp[i][j] = getOperations(i-1, j-1, s1, s2, dp);
+        
+        return dp[i][j] = 1+min(getOperations(i-1,j,s1,s2,dp), min(getOperations(i, j-1, s1, s2, dp), getOperations(i-1, j-1, s1, s2, dp)));
+        
+        
+        
+    }
+        
+        
+    int minDistance(string word1, string word2) {
+        
+        int n = word1.size();
+        int m = word2.size();
+        
+        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+        
+        return getOperations(n, m, word1, word2, dp);
+        
+    }
+};
