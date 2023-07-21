7eeec674f114129f440f7889a73082a8b849ea3d
Somyadeep Shrivastava <40947644+smdp2000@users.noreply.github.com>
2023-07-21 10:54:20 -0700

    Time: 2902 ms (5.26%), Space: 484.7 MB (6.27%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,30 @@
+int cmp(vector<int>a, vector<int>b){
+    
+    if(a[0]==b[0]) return a[1]>b[1];
+    
+    
+    return a[0]<b[0];
+    
+}
+
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), cmp);
+        
+        vector<int>ans;
+        ans.push_back(envelopes[0][1]);
+        
+        for(int i=1; i<envelopes.size(); i++){
+               int ele = envelopes[i][1];
+            
+            if(ele>ans.back())
+                    ans.push_back(ele);
+            else{
+                int index = lower_bound(ans.begin(), ans.end(), ele) - ans.begin();
+                ans[index] = ele;
+            }     
+        }   
+            return ans.size();               
+    }
+};
