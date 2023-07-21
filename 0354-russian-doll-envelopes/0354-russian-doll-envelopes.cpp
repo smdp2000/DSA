bb6e703953d5ecc71102f5fc0800fe863537ecf1
Somyadeep Shrivastava <40947644+smdp2000@users.noreply.github.com>
2023-07-21 11:11:41 -0700

    Time: 406 ms (74.80%), Space: 77.4 MB (98.46%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,9 +1,4 @@
-
-
-class Solution {
-public:
-    
-    static bool cmp(vector<int>&a, vector<int>&b){
+bool static cmp(vector<int>&a, vector<int>&b){
     
     if(a[0]==b[0]) return a[1]>b[1];
     
@@ -11,20 +6,25 @@ public:
     return a[0]<b[0];
     
 }
-    
-    int maxEnvelopes(vector<vector<int>>& env) {
-        sort(env.begin(), env.end(), cmp);
-        vector<int> lis;
+
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), cmp);
         
-        for(int i = 0;i<env.size();i++){
-            int ele = env[i][1];
-            
-            int idx = lower_bound(lis.begin(), lis.end(), ele) - lis.begin();
-            
-            if(idx >= lis.size()) lis.push_back(ele);
-            else lis[idx] = ele;
-        }
+        vector<int>ans;
+        ans.push_back(envelopes[0][1]);
         
-        return lis.size();           
+        for(int i=1; i<envelopes.size(); i++){
+               int ele = envelopes[i][1];
+            
+            if(ele>ans.back())
+                    ans.push_back(ele);
+            else{
+                int index = lower_bound(ans.begin(), ans.end(), ele) - ans.begin();
+                ans[index] = ele;
+            }     
+        }   
+            return ans.size();               
     }
 };
